6c429b36ecc6ffcf2d0834338106d78f663939a1
Daniel Alberto Vera Angulo <danielveraangulo703@gmail.com>
2024-06-06 17:10:47 -0500

    Update form of contact

--- src/app/Components/Contact/form/form.component.ts
+++ src/app/Components/Contact/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, Validators, FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ContactService } from 'src/app/Services/Contact/contact.service';
 
@@ -9,17 +9,39 @@ import { ContactService } from 'src/app/Services/Contact/contact.service';
   styleUrls: ['./form.component.css']
 })
 export class CreateComponent implements OnInit {
-  formContact: FormGroup = new FormGroup({
-    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
-    last_name: new FormControl('', [Validators.required]),
-    company: new FormControl('')
+  formContact: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    last_name: ['', [Validators.required]],
+    company: [''],
+    phones: this.fb.array([
+      this.fb.group({
+        phone: ['', [Validators.required]]
+      })
+    ]),
+    emails: this.fb.array([
+      this.fb.group({
+        email: ['', [Validators.required]]
+      })
+    ]),
+    addresses: this.fb.array([
+      this.fb.group({
+        street: ['', [Validators.required]],
+        between_streets: [''],
+        zip: [''],
+        city: [''],
+        num_ext: [''],
+        num_int: [''],
+        state: [''],
+      })
+    ])
   });
   contactId?: number;
 
   constructor(
     private router: Router,
     private contactService: ContactService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private fb: FormBuilder
   ) {
   }
 
@@ -64,11 +86,85 @@ export class CreateComponent implements OnInit {
     return this.contactId ? 'Editar contacto' : 'Nuevo contacto';
   }
 
+  get phones() {
+    return this.formContact.get('phones') as FormArray;
+  }
+
+  get emails() {
+    return this.formContact.get('emails') as FormArray;
+  }
+
+  get addresses() {
+    return this.formContact.get('addresses') as FormArray;
+  }
+
+  addPhone() {
+    this.phones.push(
+      this.fb.group({
+        phone: ['', [Validators.required]]
+      })
+    );
+  }
+
+  addEmail() {
+    this.emails.push(
+      this.fb.group({
+        email: ['', [Validators.required, Validators.email]]
+      })
+    );
+  }
+
+  addAddress() {
+    this.addresses.push(
+      this.fb.group({
+        street: ['', [Validators.required]],
+        between_streets: [''],
+        zip: [''],
+        city: [''],
+        num_ext: [''],
+        num_int: [''],
+        state: [''],
+      })
+    );
+  }
+
+  deletePhone(i: number) {
+    this.phones.removeAt(i);
+  }
+
+  deleteEmail(i: number) {
+    this.emails.removeAt(i);
+  }
+
+  deleteAddress(i: number) {
+    this.addresses.removeAt(i);
+  }
+
   private loadDataContactForm() : void {
     this.contactId = Number(this.route.snapshot.paramMap.get('id'));
 
     if (this.contactId) {
-      this.contactService.getContact(this.contactId).subscribe(contact => this.formContact.patchValue(contact));
+      this.contactService.getContact(this.contactId).subscribe(contact => {
+        if (contact.phones?.length && contact.phones.length >= 2) {
+          for(let i = 1; i <= contact.phones.length - 1; i++) {
+            this.addPhone();
+          }
+        }
+
+        if (contact.emails?.length && contact.emails.length >= 2) {
+          for(let i = 1; i <= contact.emails.length - 1; i++) {
+            this.addEmail();
+          }
+        }
+
+        if (contact.addresses?.length && contact.addresses.length >= 2) {
+          for(let i = 1; i <= contact.addresses.length - 1; i++) {
+            this.addAddress();
+          }
+        }
+
+        this.formContact.patchValue(contact)
+      });
     }
   }
 }
--- src/app/Models/contact.model.ts
+++ src/app/Models/contact.model.ts
@@ -3,6 +3,9 @@ export interface Contact {
   name: string;
   last_name?: string;
   company?: string;
+  phones?: string[];
+  emails?: string[];
+  addresses?: string[];
   created_at?: string;
   updated_at?: string;
 }
